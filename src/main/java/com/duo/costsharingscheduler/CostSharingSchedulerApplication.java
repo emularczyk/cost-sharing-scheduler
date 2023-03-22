21f473fcadcb89439ece1cf090cc9ff7fc2e9288
Emi <emilia.mularczyk00@gmail.com>
2023-03-22 18:38:18 +0100

    Project structure

---
+++ src/main/java/com/duo/costsharingscheduler/CostSharingSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.duo.costsharingscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostSharingSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostSharingSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duo/costsharingscheduler/CostSharingSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duo.costsharingscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostSharingSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
